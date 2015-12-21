30065ff0bbb5d23bc8e0b767dcd82767fbd7be94
Gustavo Ceccon <gustavofceccon@live.com>
2015-12-20 22:05:09 -0200

    Animation basic, simple controller to test

---
+++ Assets/Controller.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterAnimator))]
+public class Controller : MonoBehaviour {
+    CharacterAnimator anim;
+    bool grounded = true;
+	// Use this for initialization
+	void Awake () {
+        anim = GetComponent<CharacterAnimator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        anim.SetVelocity(Input.GetAxisRaw("Horizontal"));
+        if(Input.GetAxis("Vertical") > 0f && grounded)
+        {
+            grounded = false;
+            anim.SetGrounded(grounded);
+            StartCoroutine("Jump");
+        }
+	}
+
+    IEnumerator Jump()
+    {
+        yield return new WaitForSeconds(1f);
+        grounded = true;
+        anim.SetGrounded(grounded);
+    }
+}
---
+++ Assets/Scripts/CharacterAnimator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class CharacterAnimator : MonoBehaviour
+{
+    private Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void SetVelocity(float vel)
+    {
+        anim.SetFloat("velocity", Mathf.Abs(vel));
+    }
+
+    public void Die()
+    {
+        anim.SetTrigger("death");
+    }
+
+    public void SetGrounded(bool grounded)
+    {
+        anim.SetBool("grounded", grounded);
+    }
+}
