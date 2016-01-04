260c0b8cb3e97ea7073e7c76ef2f3a21f26c9656
Ítalo <xitsilvax@hotmail.com>
2016-01-04 12:31:08 -0300

    object dragging partially implemented

--- Assets/Scripts/my_character_controller.cs
+++ Assets/Scripts/my_character_controller.cs
@@ -5,7 +5,8 @@ using UnityStandardAssets.CrossPlatformInput;
 [RequireComponent(typeof(my_platformer_movement))]
 public class my_character_controller : MonoBehaviour {
 
-	public bool jumpPressed;
+	private bool jumpPressed;
+	public bool grabbing;
 	private my_platformer_movement character;
 
 	// Na inicializacao, pega o script com as funcoes de movimento
@@ -16,13 +17,15 @@ public class my_character_controller : MonoBehaviour {
 	private void Update () {
 		// Le o input do botao de pulo de maneira que pegue apenas uma vez a cada pressionamento do botao
 		if(!jumpPressed) jumpPressed = CrossPlatformInputManager.GetButtonDown ("Jump");
+		// Analisa se o botao left ctrl esta sendo pressionado
+		grabbing = Input.GetKey (KeyCode.LeftControl);
 	}
 
 	private void FixedUpdate(){
 		// Le o input de movimento
 		float x_axis = CrossPlatformInputManager.GetAxis ("Horizontal");
 		// Chama a funcao de movimento
-		character.move (x_axis, jumpPressed);
+		character.move (x_axis, jumpPressed, grabbing);
 		// Muda jumpPressed para false para que o input seja analisado novamente
 		jumpPressed = false;
 	}
--- Assets/Scripts/my_platformer_movement.cs
+++ Assets/Scripts/my_platformer_movement.cs
@@ -6,11 +6,9 @@ public class my_platformer_movement : MonoBehaviour {
 	public float maxFallingSpeed = 80f;
 	public float jump_speed = 15f;
 	private bool facingRight;
-	public bool hitHead, grounded;
-	// Nao usei mas ta ae
-	private Animator animator;
-	private Transform topHead;
-	private Transform feet;
+	public bool hitHead, grounded, grabAvailable;
+	private Animator animator; // Nao usei mas ta ae
+	private Transform topHead, feet;
 	private Rigidbody2D playerRigidBody;
 
 	void Awake () {
@@ -24,14 +22,40 @@ public class my_platformer_movement : MonoBehaviour {
 	}
 	
 	void raycasting(){
-		// Desenha duas linhas saindo da cabeça e dos pes
+		// Desenha tres linhas saindo da cabeça e tres dos pes
 		Debug.DrawLine (topHead.position, new Vector2 (topHead.position.x, topHead.position.y + 0.1f), Color.green);
+		Debug.DrawLine (new Vector2 (topHead.position.x-0.5f, topHead.position.y), new Vector2 (topHead.position.x-0.5f, topHead.position.y + 0.1f), Color.green);
+		Debug.DrawLine (new Vector2 (topHead.position.x+0.5f, topHead.position.y), new Vector2 (topHead.position.x+0.5f, topHead.position.y + 0.1f), Color.green);
 		Debug.DrawLine (feet.position, new Vector2 (feet.position.x, feet.position.y - 0.1f), Color.green);
-		// Se essas linhas colidirem com uma layer denominada ground (ou ainda Platform no caso dos pes) armazena numa variavel que houve colisao
-		hitHead = Physics2D.Raycast(topHead.position, Vector2.down, 0.1f, 1 << LayerMask.NameToLayer ("Ground"));
-		grounded = Physics2D.Raycast(feet.position, Vector2.down, 0.1f, 1 << LayerMask.NameToLayer ("Ground"));
-		if(!grounded)
-			grounded = Physics2D.Raycast (feet.position, Vector2.down, 0.1f, 1 << LayerMask.NameToLayer ("Platform"));
+		Debug.DrawLine (new Vector2 (feet.position.x-0.5f, feet.position.y), new Vector2 (feet.position.x-0.5f, feet.position.y - 0.1f), Color.green);
+		Debug.DrawLine (new Vector2 (feet.position.x+0.5f, feet.position.y), new Vector2 (feet.position.x+0.5f, feet.position.y - 0.1f), Color.green);
+		// Se essas linhas colidirem com uma layer denominada ground (ou ainda Platform ou Object no caso dos pes) armazena numa variavel que houve colisao
+		hitHead = Physics2D.Raycast(new Vector2(topHead.position.x-0.5f, topHead.position.y), Vector2.down, 0.1f, 1 << LayerMask.NameToLayer ("Ground"));
+		if (!hitHead) hitHead = Physics2D.Raycast(topHead.position, Vector2.down, 0.1f, 1 << LayerMask.NameToLayer ("Ground"));
+		if (!hitHead) hitHead = Physics2D.Raycast(new Vector2(topHead.position.x+0.5f, topHead.position.y), Vector2.down, 0.1f, 1 << LayerMask.NameToLayer ("Ground"));
+
+		grounded = Physics2D.Raycast(new Vector2(feet.position.x-0.5f, feet.position.y), Vector2.down, 0.1f, (1 << LayerMask.NameToLayer ("Ground")) | (1 << LayerMask.NameToLayer("Platform")) | (1 << LayerMask.NameToLayer("Object")) );
+		if (!grounded) grounded = Physics2D.Raycast(feet.position, Vector2.down, 0.1f, (1 << LayerMask.NameToLayer ("Ground")) | (1 << LayerMask.NameToLayer("Platform")) | (1 << LayerMask.NameToLayer("Object")) );
+		if (!grounded) grounded = Physics2D.Raycast(new Vector2(feet.position.x+0.5f, feet.position.y), Vector2.down, 0.1f, (1 << LayerMask.NameToLayer ("Ground")) | (1 << LayerMask.NameToLayer("Platform")) | (1 << LayerMask.NameToLayer("Object")) );
+		// Analisa a posicao do player para saber de onde os raios deve ser criados
+		Vector2 playerPosition = playerRigidBody.position;
+		// Analisa se os raios devem ser criados pelo lado esquerdo ou direito
+		playerPosition.x += (facingRight) ? 0.5f : (-1) * 0.5f;
+		// Desenha as 3 linhas para facilitar debug
+		Debug.DrawLine (new Vector2 (playerPosition.x, playerPosition.y + 0.5f), new Vector2 (playerPosition.x + (facingRight ? 0.5f : (-1)*0.5f), playerPosition.y + 0.5f), Color.green);
+		Debug.DrawLine (new Vector2 (playerPosition.x, playerPosition.y), new Vector2 (playerPosition.x + (facingRight ? 0.5f : (-1)*0.5f), playerPosition.y), Color.green);
+		Debug.DrawLine (new Vector2 (playerPosition.x, playerPosition.y - 0.5f), new Vector2 (playerPosition.x + (facingRight ? 0.5f : (-1)*0.5f), playerPosition.y - 0.5f), Color.green);
+		// Checa as tres linhas, de cima para baixo, ate que uma delas encontre um objeto
+		playerPosition.y += 0.5f;
+		grabAvailable = Physics2D.Raycast (playerPosition, (facingRight) ? Vector2.right : Vector2.left, 0.5f, 1 << LayerMask.NameToLayer ("Object"));
+		if (!grabAvailable) {
+			playerPosition.y -= 0.5f;
+			grabAvailable = Physics2D.Raycast (playerPosition, (facingRight) ? Vector2.right : Vector2.left, 0.5f, 1 << LayerMask.NameToLayer ("Object"));
+		}
+		if (!grabAvailable) {
+			playerPosition.y -= 0.5f;
+			grabAvailable = Physics2D.Raycast (playerPosition, (facingRight) ? Vector2.right : Vector2.left, 0.5f, 1 << LayerMask.NameToLayer ("Object"));
+		}
 	}
 
 	// Chama a funcao de raycasting acima
@@ -41,7 +65,7 @@ public class my_platformer_movement : MonoBehaviour {
 
 	// Funcao para realizar o giro
 	void Flip(){
-		// Alguem me explica pq exatamente isso e necessario pls (tava no script da unity)
+		// Faz com que os objetos ligados ao player mudem de lado em relaçao ao eixo x
 		Vector3 aux = transform.localScale;
 		aux.x *= -1;
 		transform.localScale = aux;
@@ -50,7 +74,9 @@ public class my_platformer_movement : MonoBehaviour {
 	}
 
 	// A variavel xMove armazena a direcao pressionada no eixo x
-	public void move(float xMove, bool jump){
+	public void move(float xMove, bool jump, bool grab){
+		// A variavel grabspeed altera a valocidade de movimento do personagem caso ele esteja segurando um objeto
+		float grabSpeed = (grab && grabAvailable) ? 0.5f : 1f;
 		// Caso nenhuma direcao esteja sendo pressionada
 		if (xMove == 0) {
 			// Se o jogador estiver no chao
@@ -66,16 +92,16 @@ public class my_platformer_movement : MonoBehaviour {
 			if(!((xMove > 0 && facingRight) || (xMove < 0 && !facingRight)))
 				Flip();
 
-			// A variavel modifier armazena um modificador, para que no ar a velocidade do jogador seja a metade da padrao
-			float modifier = (grounded)? 1f : 0.5f;
+			// A variavel airspeed armazena um modificador, para que no ar a velocidade do jogador seja tres quartos da padrao
+			float airSpeed = (grounded)? 1f : 0.75f;
 			// Caso seja pressionado para movimentar-se a direita
 			if(xMove > 0){
 				// Altera a velocidade com valor positivo
-				playerRigidBody.velocity = new Vector2(maxMoveSpeed*modifier, playerRigidBody.velocity.y);
+				playerRigidBody.velocity = new Vector2(maxMoveSpeed*airSpeed*grabSpeed, playerRigidBody.velocity.y);
 			// E caso seja pressionado para movimentar-se a esquerda
 			}else{
 				// Altera a velocidade com valor negativo
-				playerRigidBody.velocity = new Vector2(maxMoveSpeed*(-1f)*modifier, playerRigidBody.velocity.y);
+				playerRigidBody.velocity = new Vector2((-1f)*maxMoveSpeed*airSpeed*grabSpeed, playerRigidBody.velocity.y);
 			}
 		}
 
@@ -89,6 +115,10 @@ public class my_platformer_movement : MonoBehaviour {
 		if(playerRigidBody.velocity.y < (-1)*maxFallingSpeed){
 			// Mantem a velocidade maxima
 			playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, (-1f)*maxFallingSpeed);
+		}
+
+		// Alterar a velocidade do objeto sendo segurado para ser igual a velocidade do player (to do)
+		if (grab && grabAvailable) {
 		}
 	}
 }
